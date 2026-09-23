c961121be148a76100eb24f041addf906c437d11
Amar Venkatesh <bodduamar@gmail.com>
2026-09-23 22:59:45 +0530

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    System.out.println("venkatesh");
+		
+		
+        System.out.println("Amar");
+		
+		
+		System.out.println("welcome java");
+		
+		
+		int a=100, b=20, c=25;
+		System.out.println(a+" "+b+" "+c);
+	
+
+    }
+}
